dcec813455b900686708abe5d4fc90d9c26beace
Aleksandr Malakhov <schwarcroza@mail.ru>
2023-01-01 11:36:43 +0300

    add bot menu

--- dispatcher/src/main/java/ru/relex/controller/TelegramBot.java
+++ dispatcher/src/main/java/ru/relex/controller/TelegramBot.java
@@ -4,11 +4,16 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @Log4j
 @Component
@@ -27,6 +32,21 @@ public class TelegramBot extends TelegramLongPollingBot {
     @PostConstruct
     public void init() {
         updateController.registerBot(this);
+
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "начать работу со мной"));
+        listOfCommands.add(new BotCommand("/stop", "приостановить работу со мной"));
+        listOfCommands.add(new BotCommand("/help", "помощь в работе со мной"));
+        listOfCommands.add(new BotCommand("/cancel", "отменить команду"));
+        listOfCommands.add(new BotCommand("/registration", "регистрация"));
+        listOfCommands.add(new BotCommand("/myphoto", "мои фотографии"));
+        listOfCommands.add(new BotCommand("/mydoc", "мои документы"));
+
+        try {
+            execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
     }
 
     @Override
